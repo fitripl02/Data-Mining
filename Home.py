5c98f37bf8bce3eb9313b23bdb5ed73bf32c2202
fitripl02 <fitripuji0212@students.unnes.ac.id>
2025-06-03 12:47:19 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Pariwisata", layout="wide")
+st.title("🎉 Selamat Datang di Dashboard Paket Wisata!")
+st.markdown("""
+Dashboard ini memiliki 3 halaman:
+1. **Halaman Awal (EDA)** - Menampilkan data dan karakteristik paket.
+2. **Hasil Clustering** - Melihat kelompok paket wisata berdasarkan tempat wisata.
+3. **Formulir Prediksi** - Memasukkan paket wisata baru dan melihat prediksi klasternya.
+""")
 
