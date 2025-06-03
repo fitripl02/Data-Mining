fe861f1c29580a873c4f19840a5e476a8e71fb2e
fitripl02 <fitripuji0212@students.unnes.ac.id>
2025-06-03 12:45:13 +0700

    Update 1_🏠Halaman Awal (EDA).py

--- Pages/1_🏠Halaman Awal (EDA).py
+++ Pages/1_🏠Halaman Awal (EDA).py
@@ -1,1 +1,20 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title("📊 Halaman EDA (Exploratory Data Analysis)")
+df = pd.read_csv("package_tourism.csv")
+
+st.write("### Cuplikan Data")
+st.dataframe(df.head())
+
+st.write("### Jumlah Paket per Kota")
+st.bar_chart(df['City'].value_counts())
+
+df['Jumlah_Tujuan'] = df.loc[:, 'Place_Tourism1':'Place_Tourism5'].notna().sum(axis=1)
+st.write("### Distribusi Jumlah Tujuan per Paket")
+fig, ax = plt.subplots()
+sns.histplot(df['Jumlah_Tujuan'], kde=True, bins=5, ax=ax)
+st.pyplot(fig)
 
